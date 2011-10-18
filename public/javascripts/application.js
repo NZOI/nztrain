6a9d321417d9629c8e746b20cf6e2a44360c5e43
Jamie McCloskey <crimbo1994@gmail.com>
2011-10-19 11:33:03 +1300

    wrestled file input css into submission

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -29,5 +29,24 @@ $(document).ready(function() {
 
         $(this).children("td").toggleClass("ui-state-highlight");
     });
+    //make buttons look good
+    $("input").button().css({
+        'margin' : '0',
+        'vertical-align' : 'top'
+    });
+    $('input:text, input:password')
+        .button()
+        .css({
+            'font' : 'inherit',
+            'color' : 'inherit',
+            'text-align' : 'left',
+            'outline' : 'none',
+            'cursor' : 'text',
+            'height' : '15px'
+        });
 
+    $("#file-form").fileinput();
+    $("select").attr("class", "ui-button ui-widget ui-state-default ui-corner-all");
+    $("select").attr("role","button");
+    $("select").attr("aria-disabled","false")
 });
