8c9e97e7bba691800fadd34845a783af124ca56a
Jamie McCloskey <crimbo1994@gmail.com>
2011-12-11 22:43:31 +1300

    changed sorting to be compatible with old ruby versions

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -16,7 +16,8 @@ class Contest < ActiveRecord::Base
   def get_high_scorers
     people = self.contest_relations.map {|cr| {:score => self.get_score(cr.user_id), :user => cr.user_id}}
     people = people.find_all {|p| User.exists?(p[:user])}
-    people.sort_by! {|a| -a[:score]}
+    people.sort! {|a,b| a[:score] <=> b[:score]}
+    people.reverse!
     limit = (self.contest_relations.size * HIGH_SCORE_LIMIT).ceil - 1
     logger.debug "initial limit is " + limit.to_s
     newLimit = limit
