d79abf9cd281ad3cf042a9e9befebe8fa86e410f
Ronald Chan <ronalchn@gmail.com>
2012-01-22 13:59:24 +1300

    disallow pagination links if another page already loading

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -9,7 +9,6 @@ $(function () {
     });
 
     $(window).bind( 'hashchange', function(e) {
-        //$('#paginated_content').css({'opacity': 0.3}); // loading visual cue
 	var img = document.createElement("IMG");
 	img.src = "images/ajax-loader.gif";
         $(img).css({'margin': '100px'});
@@ -17,6 +16,8 @@ $(function () {
         $(div).append(img);
         $(div).css({'position': 'absolute', 'left': 0, 'top': 0, 'width': '100%', 'height': '100%', 'background-color': 'rgba(255,255,255,0.7)', 'text-align': 'center', 'opacity': 0.9});
 	$('#paginated_content').append(div);
+	$('.pagination a').css({'opacity': 0.5, 'cursor': 'default'});
+	$('.pagination a').click(function() { e.preventDefault(); return false; })
 
         // scroll to top of paginated_section if it is not visible
         if ($('body').scrollTop() > $('#paginated_section').offset().top) {
