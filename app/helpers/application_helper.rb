e88bbcd6ca0314ec9e3ecce60a62ca6861c6d621
Ronald Chan <ronalchn@gmail.com>
2012-01-31 20:40:04 +1300

    nice progress bar for problems to show score (also showing on problem sets)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 module ApplicationHelper
+  def progress_bar(percent, link = nil)
+    percent = percent.to_i
+    if link != nil
+      text = link_to((percent.to_s + '%'),link,{:class => "progress_link", :style => "display: block; width: 100%;"})
+    else
+      text = percent.to_s + '%'
+    end
+    if percent <= 50 # make a nice spectrum of colours
+      colour = "rgb(255,#{(percent*255/50).to_i},0)"
+    else
+      colour = "rgb(#{255-((percent-50)*255/50).to_i},#{255-((percent-50)*60/50).to_i},0)"
+    end
+    raw "<div style=\"display: inline-block; vertical-align: middle; position: relative; height: 18px; width: 100px; border: 1px #{colour} solid;\"><div style=\"height: 100%; width: #{percent}%; background-color: #{colour}\"></div><div style=\"position: absolute; top: 0; left: 0; width: 100%; height: 100%; text-align: center; font-size: small; text-shadow: 1px 1px #FFFFFF;\">#{text}</div></div>"
+  end
+
+
   # TODO FIXME escape_javascript override
   # https://github.com/rails/rails/pull/1558
   # REMOVE THIS MONKEY PATCH WHEN RAILS UPGRADED
