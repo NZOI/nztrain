738a596a7b1e2b830bae5b4a219540c3092d218d
Ronald Chan <ronalchn@gmail.com>
2012-02-05 13:34:11 +1300

    tested assets in production, fixed - rake assets:precompile to get css and js compressed => fixed some css incompatibilities (malformed css), set serve_static_assets = true so can test production mode in Webrick..... TODO: fix timezone.rb TimeWithZone. Also noticed put/delete links never worked when javascript disabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ NztrainV2::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true # true so Webrick testing of production mode works
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -49,9 +49,11 @@ NztrainV2::Application.configure do
 
   # Compress JavaScript and CSS  
   config.assets.compress = true  
-     
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglifier
+ 
   # Don't fallback to assets pipeline  
-  config.assets.compile = false  
+  # config.assets.compile = false  
      
   # Generate digests for assets URLs  
   config.assets.digest = true  
