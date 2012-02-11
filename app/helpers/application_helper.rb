8cbfe0517f59d62aa659f4f2ac30ed0d8e7195ff
Ronald Chan <ronalchn@gmail.com>
2012-02-11 20:23:30 +1300

    remove the escape javascript overriding monkey patch, because it got fixed in newer version of rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,16 +17,4 @@ module ApplicationHelper
   def in_su?
     session[:su] && !session[:su].empty?
   end
-
-  # TODO FIXME escape_javascript override
-  # https://github.com/rails/rails/pull/1558
-  # REMOVE THIS MONKEY PATCH WHEN RAILS UPGRADED
-  JS_ESCAPE_MAP	=	{ '\\' => '\\\\', '</' => '<\/', "\r\n" => '\n', "\n" => '\n', "\r" => '\n', '"' => '\\"', "'" => "\\'" } 
-  def escape_javascript(javascript)
-    if javascript
-      javascript.gsub(/(\\|<\/|\r\n|[\n\r"'])/) { |s| JS_ESCAPE_MAP[s] }
-    else
-      ''
-    end 
-  end
 end
