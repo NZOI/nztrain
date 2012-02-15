20ddea78b8d728fbbe46624c0ebd513e714a6acc
Ronald Chan <ronalchn@gmail.com>
2012-02-15 16:36:33 +1300

    add mkdir of uploads directory to update.sh for symlinks to work

--- update.sh
+++ update.sh
@@ -11,21 +11,28 @@ do
     esac
 done
 
+# change pwd to RAILS ROOT (the directory this script resides in)
 
 # note, we are bashing the echoes so that "sh update.sh" will still print coloured output
 
 # optional: mark website down (.htaccess url rewrite or other similar)
+
 if ${gitpull:=true} ; then
     # pull from git
     bash -c "echo -e '\E[34m\033[1mgit pull\033[0m'"
     git pull | sed -e 's/^/   /g;' | cat # sed to indent output
 fi
 
+# make directories for storage
+cmd="mkdir db/data/uploads/user/avatar/ -p"
+bash -c "echo -e '\E[34m\033[1m$cmd\033[0m'"
+$cmd
+
 # install new gems required
 bash -c "echo -e '\E[34m\033[1mbundle install\033[0m'"
 bundle install | sed -e 's/^/   /g;/Using/{:a;N;$!ba;s/Using [0-9a-z_ -]\+\(([0-9.]\+) \)\?\n//g}' | cat # sed indents, remove output of un-upgraded gems
 
-# Stop WEBrick/Apache server here
+# Stop WEBrick/Apache server here (only if database needs migration?)
 
 # migrate database
 bash -c "echo -e '\E[34m\033[1mbundle exec rake db:migrate\033[0m'"
@@ -34,10 +41,13 @@ bundle exec rake db:migrate | sed -e 's/^/   /g;' | cat # sed to indent output
 bash -c "echo -e '\E[34m\033[1mbundle exec rake db:seed\033[0m'"
 bundle exec rake db:seed | sed -e 's/^/   /g;' | cat # sed to indent output
 
+# can restart server here
+
+# update cron jobs
 bash -c "echo -e '\E[34m\033[1mbundle exec whenever --update-crontab nztrain\033[0m'"
 bundle exec whenever --update-crontab nztrain
 
-# precompile assets
+# precompile assets (if in production mode)
 bash -c "echo -e '\E[34m\033[1mbundle exec rake assets:precompile\033[0m'"
 bundle exec rake assets:precompile | sed -e 's/^/   /g;' | cat # sed to indent output
 
