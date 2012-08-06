1f415125cf86e6962bba8010ccb980c66d3f29d7
Ronald Chan <ronalchn@gmail.com>
2012-08-07 11:13:45 +1200

    don't force access Setting table (in case on travis rake:db:prepare task)

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,5 +1,7 @@
 # use db stored username and password for email if present
 
-ActionMailer::Base.smtp_settings[:user_name] = Setting.find_by_key("system/mailer/username").value if Setting.find_by_key("system/mailer/username")
-ActionMailer::Base.smtp_settings[:password] = Setting.find_by_key("system/mailer/password").value if Setting.find_by_key("system/mailer/password")
+if ActiveRecord::Base.connection.table_exists?(Setting.table_name)
+  ActionMailer::Base.smtp_settings[:user_name] = Setting.find_by_key("system/mailer/username").value if Setting.find_by_key("system/mailer/username")
+  ActionMailer::Base.smtp_settings[:password] = Setting.find_by_key("system/mailer/password").value if Setting.find_by_key("system/mailer/password")
+end
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,7 +8,7 @@ NztrainV2::Application.config.secret_token = '9c912a91a69a2b65a0de03c7c96f83c38b
 
 # if a setting is stored in database, use that secret token instead (because that token is not checked in to the git repository)
 # only appears to affect cookie stored sessions (no apparent effect on database stored sessions)
-if Setting.exists?(:key => "sessions/secret_token")
+if ActiveRecord::Base.connection.table_exists?(Setting.table_name) && Setting.exists?(:key => "sessions/secret_token")
   token = Setting.find_by_key("sessions/secret_token").value
   NztrainV2::Application.config.secret_token = token if token && !(token.empty?)
 end
