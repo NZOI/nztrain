f728d39845508340e9925c7ae595416b52c1d3db
Ronald Chan <ronalchn@gmail.com>
2012-08-11 19:19:12 +1200

    test cases

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem 'pg'
 group :development, :test do
   gem 'webrat'
 
+  gem 'factory_girl_rails', '~> 2.0'
+  gem 'factory_girl', '~> 2.0'
+
   # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
   gem 'ruby-debug'
   # gem 'ruby-debug19', :require => 'ruby-debug'
--- test/unit/contest_score_test.rb
+++ test/unit/contest_score_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ContestScoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    relation = ContestRelation.where(:user => users(:normal_user), :contest => contests(:contestscore))
+    assert relation.first.score == 0
+  end
+
+  test "the truth" do
+    assert true
+  end
+
+  test "scores only during contest relation" do
+    Submission.new(submissions(:sub2beforecontest).merge(:))
+  end
 end
