bfd8e138db7e2995f0e6c9892ec6ae9253fefec2
Ronald Chan <ronalchn@gmail.com>
2013-12-17 18:13:36 +1300

    make flat case importer compatible with COCI test cases

--- app/importers/problems/flat_case_importer.rb
+++ app/importers/problems/flat_case_importer.rb
@@ -1,17 +1,27 @@
 module Problems
   class FlatCaseImporter < BaseImporter
     def import(path, options = {})
+      setmap = {}
       dir.foreach(path) do |entry|
+        setname = nil
         outputname = case entry
         when /.in\z/; "#{name = entry.chomp('.in')}.out"
         when /.IN\z/; "#{name = entry.chomp('.IN')}.OUT"
         when /.i\z/; "#{name = entry.chomp('.i')}.o"
         when /(.*)\.in((put)?\.(.*))\z/
-          name = $~[1] + $~[2]
-          "#{$~[1]}.out#{$~[2]}"
+          n1, n2 = $~[1,2]
+          name = n1 + n2
+          # for COCI compatibility
+          if n1 =~ /\.(dummy|sample)(\.|\z)/
+            setname = n1
+          elsif n2 =~ /\A(\.[[:digit:]]+)[[:alpha:]]+\z/
+            setname = n1 + $~[1]
+          end
+          "#{n1}.out#{n2}"
         else
           nil
         end
+        setname ||= name
         if outputname && file.exist?(ofile = File.expand_path(outputname, path))
           caseopts = {:input => file.read(File.expand_path(entry, path)), :output => file.read(ofile)}
           if casemap.has_key?(name)
@@ -19,11 +29,17 @@ module Problems
           else
             kase = TestCase.new(caseopts.merge(:name => name))
             problem.test_cases << kase
-            setopts = {:name => name, :points => 1, :test_cases => [kase]}
-            if name =~ /\.(dummy|sample)(\.|\z)/
-              setopts.merge(:points => 0, :visibility => :sample)
+            if setmap.has_key?(setname)
+              set = setmap[setname]
+            else
+              setopts = {:name => setname, :points => 1}
+              if setname =~ /\.(dummy|sample)(\.|\z)/
+                setopts.merge!(:points => 0, :visibility => :sample)
+              end
+              set = setmap[setname] = TestSet.new(setopts)
+              problem.test_sets << set
             end
-            problem.test_sets << TestSet.new(setopts)
+            set.test_cases << kase
             casemap[name] = kase
           end
         end
