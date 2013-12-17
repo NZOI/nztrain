2b304c00ac3cc17f5b17dfeb8a26149a343683a3
Ronald Chan <ronalchn@gmail.com>
2013-12-17 17:02:16 +1300

    rename association

--- app/controllers/groups/files_controller.rb
+++ app/controllers/groups/files_controller.rb
@@ -11,21 +11,21 @@ class Groups::FilesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     authorize @group, :access?
-    @files = @group.group_file_attachments.order(:filepath)
+    @files = @group.file_attachments.order(:filepath)
     @new_file = GroupFileAttachment.new
   end
 
   def show
     @group = Group.find(params[:group_id])
     authorize @group, :access?
-    @file = @group.group_file_attachments.find(params[:id])
+    @file = @group.file_attachments.find(params[:id])
     send_file FileAttachmentUploader.root + @file.file_attachment_url, :filename => File.basename(@file.filepath), :disposition => 'inline'
   end
 
   def update
     @group = Group.find(params[:group_id])
     authorize @group, :update?
-    @file = @group.group_file_attachments.find(params[:id])
+    @file = @group.file_attachments.find(params[:id])
     if @file.update_attributes(group_file_attachment_params)
       redirect_to(group_files_path(@group), :notice => "File attachment updated")
     else
@@ -36,11 +36,11 @@ class Groups::FilesController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     authorize @group, :update?
-    @new_file = @group.group_file_attachments.build(group_file_attachment_params)
+    @new_file = @group.file_attachments.build(group_file_attachment_params)
     if @new_file.save
       redirect_to(group_files_path(@group), :notice => "File attachment added")
     else
-      @files = @group.group_file_attachments.order(:filepath)
+      @files = @group.file_attachments.order(:filepath)
       render :action => :index
     end
   end
@@ -48,8 +48,8 @@ class Groups::FilesController < ApplicationController
   def destroy
     @group = Group.find(params[:group_id])
     authorize @group, :update?
-    @file = @group.group_file_attachments.find(params[:id])
-    if @group.group_file_attachments.destroy(@file)
+    @file = @group.file_attachments.find(params[:id])
+    if @group.file_attachments.destroy(@file)
       redirect_to(group_files_path(@group), :notice => "File attachment removed")
     else
       redirect_to(group_files_path(@group), :notice => "File attachment not removed")
