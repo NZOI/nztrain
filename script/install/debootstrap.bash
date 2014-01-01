9054e3e4b83f71290bcdcef129cfe5411469dfc4
Ronald Chan <ronalchn@gmail.com>
2014-01-01 19:50:09 +1300

    c++11 support

--- script/install/debootstrap.bash
+++ script/install/debootstrap.bash
@@ -46,7 +46,18 @@ if ${update:=true} || ${new_debootstrap:=true} ; then
   chroot "$ISOLATE_ROOT" apt-get update
 fi
 
-chroot_install="$ chroot \"$ISOLATE_ROOT\" apt-get install"
+chroot_cmd="$ chroot \"$ISOLATE_ROOT\""
+chroot_install="$chroot_cmd apt-get install"
+
+echo "$chroot_cmd apt-get update"
+chroot "$ISOLATE_ROOT" apt-get update
+
+echo "$chroot_install software-properties-common"
+chroot "$ISOLATE_ROOT" apt-get install software-properties-common # provides add-apt-repository
+
+# only for <= 12.04
+echo "$chroot_install python-software-properties"
+chroot "$ISOLATE_ROOT" apt-get install python-software-properties # provides add-apt-repository
 
 echo "$chroot_install build-essential"
 chroot "$ISOLATE_ROOT" apt-get install build-essential # C/C++ (g++, gcc)
@@ -66,5 +77,26 @@ if [ ! -f "$ISOLATE_ROOT/usr/bin/cint.rb" ] ; then
   echo "$cmd"
   $cmd
 fi
+
+# gcc 4.8.1 on Precise (will not be needed when we use trusty tahr)
+echo "$chroot_cmd add-apt-repository ppa:ubuntu-toolchain-r/test"
+chroot "$ISOLATE_ROOT" add-apt-repository ppa:ubuntu-toolchain-r/test
+
+echo "$chroot_cmd apt-get update"
+chroot "$ISOLATE_ROOT" apt-get update
+
+echo "$chroot_cmd apt-get install gcc-4.8"
+chroot "$ISOLATE_ROOT" apt-get install gcc-4.8
+
+echo "$chroot_cmd apt-get install g++-4.8"
+chroot "$ISOLATE_ROOT" apt-get install g++-4.8
+
+echo "$chroot_cmd update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50"
+chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+
+echo "$chroot_cmd update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50"
+chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+# gcc 4.8.1 done
+
 # let user know that chroot installs are finished
 
