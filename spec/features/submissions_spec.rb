6622d137e0d90d7cf9275f9f57ce9abec7115b87
Ronald Chan <ronalchn@gmail.com>
2014-01-01 23:41:24 +1300

    fix spec for capybaras weird select option finding behaviour

--- spec/features/submissions_spec.rb
+++ spec/features/submissions_spec.rb
@@ -10,7 +10,7 @@ feature 'submission' do
     
     expect do
       within '#new_submission' do
-        select LanguageGroup.find_by_identifier("c++").current_language.id, :from => 'submission_language_id'
+        find(:select,'submission_language_id').find(:xpath, "//option[@value=#{LanguageGroup.find_by_identifier("c++").current_language.id}]").select_option
         attach_file 'submission_source_file', Rails.root.join('spec/fixtures/files/adding.cpp')
         click_on 'Submit'
       end
