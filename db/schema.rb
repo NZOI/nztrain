ddee44b9a379102f4826a45828821fb7b449f110
Ronald Chan <ronalchn@gmail.com>
2015-01-09 23:50:35 +1300

    db migrate for schools

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107090450) do
+ActiveRecord::Schema.define(version: 20150109101859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -402,6 +402,9 @@ ActiveRecord::Schema.define(version: 20150107090450) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.datetime "last_seen_at"
+    t.integer  "school_id"
+    t.string   "country_code",           limit: 3
+    t.date     "school_graduation"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
