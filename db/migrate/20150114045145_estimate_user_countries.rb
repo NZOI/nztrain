65cae31c335268e73b79aaac52f48c443c046089
Ronald Chan <ronalchn@gmail.com>
2015-01-16 11:29:10 +1300

    Update 20150114045145_estimate_user_countries.rb

--- db/migrate/20150114045145_estimate_user_countries.rb
+++ db/migrate/20150114045145_estimate_user_countries.rb
@@ -2,7 +2,7 @@ class EstimateUserCountries < ActiveRecord::Migration
   def up
     User.find_each do |u|
       ip = u.current_sign_in_ip || u.last_sign_in_ip
-      if ip
+      if u.country_code.nil? && ip
         u.country_code = Geocoder.search(ip).first.try(:country_code)
         u.save
       end
