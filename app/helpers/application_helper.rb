52b4ebcc486ad1e545838308024bb3b7e0ab3f1d
Ronald Chan <ronalchn@gmail.com>
2015-02-05 00:23:15 +1300

    show weighting with - (no submission) in progress bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,7 +47,7 @@ module ApplicationHelper
         end
       end
     else
-      text = '-'
+      text = weighting.nil? ? '-' : "-&nbsp;/#{weighting}"
     end
     if link != nil
       text = link_to(text,link,{:class => "progress_link", :style => "display: block; width: 100%;"})
