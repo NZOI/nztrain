6f3825fce3f4ff0c677c3d60ee2ce665cc0d5efa
Ronald Chan <ronalchn@gmail.com>
2015-03-03 23:26:19 +1300

    fix admin show bug when competing

--- app/policies/problem_set_policy.rb
+++ app/policies/problem_set_policy.rb
@@ -19,7 +19,7 @@ class ProblemSetPolicy < ApplicationPolicy
   end
 
   def show?
-    scope.where(:id => record.id).exists?
+    return true if user.is_staff?
     return user.contest_relations.where{|relation| (relation.started_at <= DateTime.now) & (relation.finish_at > DateTime.now) & relation.contest_id >> record.contest_ids }.exists? if user.competing?
     user.owns(record) or record.groups.where(:id => 0).exists? or record.group_memberships.where{|membership|(membership.member_id == user.id)}.exists?
   end
