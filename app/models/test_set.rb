e16353199c0c4ec43f0e79932cdb1866be23091b
Ronald Chan <ronalchn@gmail.com>
2016-01-24 16:50:23 +1300

    fix order

--- app/models/test_set.rb
+++ app/models/test_set.rb
@@ -3,7 +3,7 @@ class TestSet < ActiveRecord::Base
 
   belongs_to :problem, inverse_of: :test_sets, touch: :rejudge_at
   has_many :test_case_relations, inverse_of: :test_set, dependent: :destroy
-  has_many :test_cases, -> { order(problem_order: :asc) }, through: :test_case_relations
+  has_many :test_cases, -> { order("problem_order asc") }, through: :test_case_relations
 
   include RankedModel
   ranks :problem_order, with_same: :problem_id
