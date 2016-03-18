492b54ca06ae9769bf4123f1da9e2bb008ea5098
Ronald Chan <ronalchn@gmail.com>
2016-03-18 19:24:08 +1300

    allow username change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -77,6 +77,10 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username << :name << :email << :country_code << :school_id << {school_graduation: [:enabled, :month, :year]} << {school: [:name, :country_code]}
+
+    if user_signed_in? && current_user.can_change_username?
+      devise_parameter_sanitizer.for(:account_update) << :username
+    end
   end
 
   def update_last_seen_at
