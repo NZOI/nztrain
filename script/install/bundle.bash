e90e2eceab53435aa1c9623394e1d368e3ae0040
Tom Levy <tomlevy93@gmail.com>
2019-02-02 20:43:17 +1300

    Don't use sudo with bundler
    Bundler will ask for sudo password if necessary.
    See https://bundler.io/man/bundle-install.1.html#SUDO-USAGE.
    
    Restore a comment about a cryptic sed script. The comment was removed in
    commit e307133 ("reorganise install/update scripts", 2013-02-07).

--- script/install/bundle.bash
+++ script/install/bundle.bash
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-sdo="sudo "
-rvm --version &>/dev/null && {
-  if [[ `rvm current` != "system" ]] ; then
-    sdo=
-  fi
-}
-
 lastupdate=`cat script/update.time` # get time of last update
 
 lastmodified=`find Gemfile Gemfile.lock -printf '%T+\n' | sort -r | head -n1`
 if [[ "$lastupdate" < "$lastmodified" ]] ; then
-  echo "$ ${sdo}bundle install"
-  ${sdo}bundle install | sed -e 's/^/   /g;/Using/{:a;N;$!ba;s/Using [0-9a-z_ -]\+\(([0-9.]\+) \)\?\n//g}' | cat || exit 1
+  # don't use sudo with bundler, it will ask for password if necessary
+  # see https://bundler.io/man/bundle-install.1.html#SUDO-USAGE
+  echo "$ bundle install"
+  bundle install | sed -e 's/^/   /g;/Using/{:a;N;$!ba;s/Using [0-9a-z_ -]\+\(([0-9.]\+) \)\?\n//g}' | cat || exit 1
+  # sed indents, remove output of un-upgraded gems
 fi
 
