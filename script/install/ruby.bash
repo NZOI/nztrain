f7bfe8b9817c44b663385ce8d24a8a0c41a3e44d
Tom Levy <tomlevy93@gmail.com>
2019-02-02 17:52:52 +1300

    Warn about using Ruby < 2.3
    - Ruby < 2.3 has reached end-of-life and is no longer supported
    
    - Gem dependencies are broken (e.g. public_suffix-3.0.1 requires
      ruby >= 2.1, bundler 2.0.1 requires ruby >= 2.3.0)
    
    - Custom evaluators only work on Ruby >= 2.3
      (need https://github.com/ruby/ruby/commit/1ade9cad02a3)
    
    Mark the install options (System MRI Ruby, RVM, rbenv) as out of date
    for now because they install Ruby < 2.3 (the user can still use them).

--- script/install/ruby.bash
+++ script/install/ruby.bash
@@ -1,14 +1,37 @@
 #!/usr/bin/env bash
 
 min_version=1.9.2
+
+# min_version needs to be increased to 2.3, but for now still allow 1.9.2 - 2.2
+# when min_version is set to 2.3 this section of the script can be removed
+min_recommended_version=2.3
+if ruby_version="$(ruby -e 'puts RUBY_VERSION' 2>/dev/null)"; then
+  echo "Ruby $ruby_version detected"
+  if echo "$ruby_version" | bash script/check_version.bash $min_recommended_version; then
+    exit 0 # new version already installed, skip installation
+  elif echo "$ruby_version" | bash script/check_version.bash $min_version; then
+    echo "Warning: This is an old version of Ruby with known problems"
+    echo "- Ruby < 2.3 has reached end-of-life and is no longer supported"
+    echo "- Gem dependencies are broken (e.g. public_suffix-3.0.1 requires ruby >= 2.1, bundler 2.0.1 requires ruby >= 2.3.0)"
+    echo "- Custom evaluators only work on Ruby >= 2.3 (need https://github.com/ruby/ruby/commit/1ade9cad02a3)"
+    if ! DEFAULT=1 bash script/confirm.bash "Use Ruby $ruby_version"; then
+      min_version=$min_recommended_version # force newer version to be installed
+    fi
+  else
+    echo "This version of Ruby is too old, you must install a new one"
+  fi
+else
+  echo "Ruby is not installed"
+fi
+
 ruby -e 'puts RUBY_VERSION' 2>/dev/null | bash script/check_version.bash $min_version || {
   echo "Ruby $min_version+ required!"
-  echo Select an install option for Ruby 1.9.3
+  echo "Select an install option for Ruby"
 
   # detect RVM
   rvm --version && rvm_option="Install Ruby using RVM" || rvm_option="Install RVM (Single User)"
   rbenv --version && rbenv_option="Install Ruby using rbenv" || rbenv_option="Install rbenv (Single User on Desktop with ruby-build)"
-  select INSTALL_OPTION in "Don't install" "System MRI Ruby from source" "$rvm_option" "$rbenv_option";
+  select INSTALL_OPTION in "Exit without installing" "System MRI Ruby from source (OUT OF DATE, DON'T USE)" "$rvm_option (OUT OF DATE, DON'T USE)" "$rbenv_option (OUT OF DATE, DON'T USE)";
   do
     case $REPLY in
       1)
