4f8cbf48949b6c5d8b465ecd2de37d276e985f54
Tom Levy <tomlevy93@gmail.com>
2019-02-02 20:42:35 +1300

    Only install 'bundler' system-wide if 'gem' was installed system-wide
    Only use sudo with 'gem install' if necessary. If 'gem' is in a user
    directory (e.g. with RVM/rbenv) sudo won't find it.
    
    Fix the path to bundler. The directory in a gem containing executables
    doesn't have to be called 'bin'[1], and for bundler it's currently
    'exe'. But the executable in that directory shouldn't be used directly
    anyway, instead use the RubyGems binstub[2] in Gem.bindir[3] (usually
    the directory containing the 'ruby' executable).
    
    [1] https://guides.rubygems.org/specification-reference/#bindir
    [2] https://github.com/rbenv/rbenv/wiki/Understanding-binstubs
    [3] https://ruby-doc.org/stdlib/libdoc/rubygems/rdoc/Gem.html#method-c-bindir

--- script/install/bundler.bash
+++ script/install/bundler.bash
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
 bundle version 2>/dev/null || {
-  cmd="sudo gem install bundler";echo "$ $cmd"
-  $cmd && {
-    sudo update-alternatives --install /usr/bin/bundle bundle `dirname \`gem which bundler\``/../bin/bundle 400
-  } || exit 1
+  # check if directory where gems are installed is writable
+  if [ -w "$(gem environment gemdir)" ]; then
+    # don't need sudo (probably user installation e.g. rvm/rbenv, or we could be running as root)
+    cmd="gem install bundler"
+    echo "$ $cmd"
+    $cmd || exit 1
+  else
+    # need to use sudo to install gems (system-wide installation)
+    cmd="sudo gem install bundler"
+    echo "$ $cmd"
+    $cmd || exit 1
+  fi
+  # check if 'gem' is available in the system-wide PATH (e.g. /usr/bin/gem)
+  # ('command -v' is like 'which', -p uses the default value of PATH)
+  if command -p -v gem &>/dev/null; then
+    # if it is, also make bundler available in the system-wide PATH
+    sudo update-alternatives --install /usr/bin/bundle bundle "$(ruby -e 'puts Gem.bindir')/bundle" 400 || exit 1
+  fi
 }
 
 
