1eea31ca1972f23aa743b70198843e48617d1435
Thomas <puqeko@gmail.com>
2020-01-24 13:38:34 +1300

    Use file descriptors for redirect and rename to C# from CSharp

--- script/csharp/compile-command.sh
+++ script/csharp/compile-command.sh
@@ -7,11 +7,11 @@
 # Print version and copyright by compiling nothing
 /usr/bin/dotnet /usr/share/dotnet/sdk/3.1.101/Roslyn/bincore/csc.dll | \
 # print the first line, which has compiler version info, to stdout
-head -n 1 > /dev/stdout
+head -n 1 >&1
 
 # Actually compile:
 # [dotnet] [csc] -reference:[stdlib.dll] [...passed in args...]
 /usr/bin/dotnet /usr/share/dotnet/sdk/3.1.101/Roslyn/bincore/csc.dll \
 -reference:/usr/share/dotnet/sdk/3.1.101/ref/netstandard.dll "$@" | \
 # Remove the first 3 lines of output. The rest, if any, are errors.
-tail -n +4 > /dev/stderr
+tail -n +4 >&2
