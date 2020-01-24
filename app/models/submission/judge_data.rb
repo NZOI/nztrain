162c0f40850bc09195afaa74272f51a69c5d4bf0
Thomas <puqeko@gmail.com>
2020-01-24 17:20:45 +1300

    Show "Compilation Error" when the compile command fails instead of "Runtime Error" (#66)

--- app/models/submission/judge_data.rb
+++ app/models/submission/judge_data.rb
@@ -106,7 +106,8 @@ class Submission
         case status
         when :timeout; "Time Limit Exceeded"
         when :walltime; "Time Limit Exceeded (Wall)"
-        when :runtime; "Runtime Error"
+        # Report runtime errors as compilation errors when executing the compile command
+        when :runtime; "Compilation Error"
         when :signal; "Fatal Signal"
         when :memory; "Memory Limit Exceeded"
         when :pending; "Pending"
