ff9746c1e231465d8f95680c998735416157207a
Thomas <puqeko@gmail.com>
2020-01-24 13:20:19 +1300

    Make sure things are copied to the chroot directory and commands are run in chroot

--- script/install/debootstrap.bash
+++ script/install/debootstrap.bash
@@ -200,15 +200,15 @@ chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g
 
   : Downloading package info from Microsoft
   UBUNTU_VERSION=$(lsb_release --release --short)
-  wget -q "https://packages.microsoft.com/config/ubuntu/${UBUNTU_VERSION}/packages-microsoft-prod.deb" -O packages-microsoft-prod.deb
-  dpkg -i packages-microsoft-prod.deb
-  rm packages-microsoft-prod.deb
-  [ "$UBUNTU_VERSION" == "18.04" ] && sudo add-apt-repository universe
-  apt-get install -y apt-transport-https
-  apt-get update
+  chroot "$ISOLATE_ROOT" wget -q "https://packages.microsoft.com/config/ubuntu/${UBUNTU_VERSION}/packages-microsoft-prod.deb" -O packages-microsoft-prod.deb
+  chroot "$ISOLATE_ROOT" dpkg -i packages-microsoft-prod.deb
+  chroot "$ISOLATE_ROOT" rm packages-microsoft-prod.deb
+  [ "$UBUNTU_VERSION" == "18.04" ] && chroot "$ISOLATE_ROOT" sudo add-apt-repository universe
+  chroot "$ISOLATE_ROOT" apt-get install -y apt-transport-https
+  chroot "$ISOLATE_ROOT" apt-get update
 
   : Installing dotnet 3.1
-  apt-get install -y dotnet-sdk-3.1
+  chroot "$ISOLATE_ROOT" apt-get install -y dotnet-sdk-3.1
 
   : Creating runtime config file
   # The dotnet command will not execute the compiled .exe unless there is a config file
@@ -225,13 +225,13 @@ chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g
 
   : Moving shell scripts into place
   
-  cp "script/csharp/compile-command.sh" "/usr/local/csc.sh"
-  ln "/usr/local/csc.sh" "/usr/bin/csc"
-  chmod +x "/usr/bin/csc"
+  cp "script/csharp/compile-command.sh" "$ISOLATE_ROOT/usr/local/csc.sh"
+  chroot "$ISOLATE_ROOT" ln "/usr/local/csc.sh" "/usr/bin/csc"
+  chroot "$ISOLATE_ROOT" chmod +x "/usr/bin/csc"
 
-  cp "script/csharp/run-command.sh" "/usr/local/csr.sh"
-  ln "/usr/local/csr.sh" "/usr/bin/csr"
-  chmod +x "/usr/bin/csr"
+  cp "script/csharp/run-command.sh" "$ISOLATE_ROOT/usr/local/csr.sh"
+  chroot "$ISOLATE_ROOT" ln "/usr/local/csr.sh" "/usr/bin/csr"
+  chroot "$ISOLATE_ROOT" chmod +x "/usr/bin/csr"
 
   set +x
 }
