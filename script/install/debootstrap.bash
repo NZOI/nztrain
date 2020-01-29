ac8d557a972b9860e17178b5c86b5f4fce817887
Tom Levy <tomlevy93@gmail.com>
2020-01-29 13:36:46 +1300

    Add comment explaining the .NET Core Runtime version

--- script/install/debootstrap.bash
+++ script/install/debootstrap.bash
@@ -217,6 +217,9 @@ chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g
   RUNTIME_CONFIG_TEMPL="script/csharp/dotnet-config-template.json"
 
   # referenced in RUNTIME_CONFIG_TEMPL file
+  # Compute the .NET Core Runtime version from the .NET Core SDK version
+  # The major and minor versions are the same (https://docs.microsoft.com/en-us/dotnet/core/versions/).
+  # The patch version should be set to 0 (https://github.com/dotnet/designs/blob/master/accepted/runtime-binding.md).
   DOTNET_FW_VERSION=$(chroot "$ISOLATE_ROOT" dotnet --version | sed 's/\.[0-9]\+$/.0/') \
     envsubst < $RUNTIME_CONFIG_TEMPL > $RUNTIME_CONFIG_PATH
 
