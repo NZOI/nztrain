09ca669bed819dc66360784db4af9ce92a43d7f5
tom93 <tomlevy93@gmail.com>
2020-02-02 23:52:02 +1300

    Update script/install/whenever.bash

--- script/install/whenever.bash
+++ script/install/whenever.bash
@@ -5,6 +5,5 @@ lastupdate=`cat script/update.time` # get time of last update
 lastmodified=`find config/schedule.rb config/backup.yml -printf '%T+\n' | sort -r | head -n1`
 if [[ "$lastupdate" < "$lastmodified" ]] ; then
   echo '$ whenever --update-crontab '`basename \`pwd\``
-  whenever --update-crontab `basename \`pwd\``
+  bundle exec whenever --update-crontab `basename \`pwd\``
 fi
-
