30ec3dab32b1280f45d77991d21f624f937e55e6
Tom Levy <tomlevy93@gmail.com>
2020-02-04 19:37:52 +1300

    Move V8 installation commands to a separate file
    This will make it easier to run them in chroot.
    
    It also fixes an issue where the current working directory is changed
    to ~ for the remainder of debootstrap.bash.

--- script/install/debootstrap.bash
+++ script/install/debootstrap.bash
@@ -177,39 +177,9 @@ chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g
   echo "If this fails, visit https://v8.dev/docs/build and follow the instructions."
   echo "This may take a while..."
   echo
-  set -x
 
-  : Making temporary directory
-  mkdir ~/v8-build
-  cd ~/v8-build
+  bash script/install/v8.bash
 
-  : Installing depot_tools
-  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-  export PATH="$PATH:$PWD/depot_tools"
-  gclient
-
-  : Getting V8 source code
-  mkdir v8
-  cd v8
-  fetch v8
-  cd v8
-
-  : Installing additional build dependencies
-  gclient sync
-  ./build/install-build-deps.sh
-
-  : Compiling V8
-  ./tools/dev/v8gen.py x64.release -- v8_use_external_startup_data=false
-  ninja -C out.gn/x64.release d8
-
-  : Copying executable into place
-  cp ./out.gn/x64.release/d8 "$ISOLATE_ROOT"/usr/bin/d8
-
-  : Cleaning up
-  cd ~
-  rm -r v8-build
-
-  set +x
   echo "JavaScript V8 installed into $ISOLATE_ROOT/usr/bin/d8"
   echo
 }
---
+++ script/install/v8.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# builds V8 (JavaScript engine) from source
+
+set -x
+
+: Making temporary directory
+mkdir ~/v8-build
+cd ~/v8-build
+
+: Installing depot_tools
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="$PATH:$PWD/depot_tools"
+gclient
+
+: Getting V8 source code
+mkdir v8
+cd v8
+fetch v8
+cd v8
+
+: Installing additional build dependencies
+gclient sync
+./build/install-build-deps.sh
+
+: Compiling V8
+./tools/dev/v8gen.py x64.release -- v8_use_external_startup_data=false
+ninja -C out.gn/x64.release d8
+
+: Copying executable into place
+cp ./out.gn/x64.release/d8 "$ISOLATE_ROOT"/usr/bin/d8
+
+: Cleaning up
+cd ~
+rm -r v8-build
+
+set +x
