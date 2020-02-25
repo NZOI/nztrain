b29d5f235b3e053e2e22ef8fcbd010ac8cc70bb0
Tom Levy <tomlevy93@gmail.com>
2020-02-26 00:50:50 +1300

    Remove unrelated commented-out lines from JRE install script (#251)
    The lines were copied from script/install/imagemagick.bash and are
    irrelevant.

--- script/install/jre.bash
+++ script/install/jre.bash
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-#min_version=6
-#convert -version 2>/dev/null | bash script/extract_version.bash | bash script/check_version.bash $min_version || { # already installed
-#  echo ImageMagick $min_version+ required!
-#  bash script/confirm.bash "Install ImageMagick" && {
-#    cmd="sudo apt-get install imagemagick"
-#    echo "$ $cmd"
-#    $cmd
-#  } || exit 1
-#}
-
 cmd="sudo apt-get install default-jre-headless" # java required by yui-compressor gem
 echo "$ $cmd"
 $cmd
