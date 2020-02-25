00b3dc3f74edcf038d7cd92422891690a9fc74b9
Tom Levy <tomlevy93@gmail.com>
2020-02-26 00:45:11 +1300

    Switch to default-jre-headless on server for yui-compressor (#251)
    Was previously openjdk-8-jdk.
    
     - yui-compressor only requires the JRE, not the full JDK
    
     - GUI libraries aren't required, headless version is enough
    
     - the JRE version doesn't really matter, use the default so it gets
       automatically updated

--- script/install.bash
+++ script/install.bash
@@ -40,7 +40,7 @@ bash script/install/nztrain.bash || exit 1 # fix files & directory structure
 
 bash script/install/bundler.bash || exit 1
 
-bash script/install/jdk.bash || exit 1 # required by yui-compressor
+bash script/install/jre.bash || exit 1 # required by yui-compressor
 
 sudo bash script/install/isolate.bash || exit 1 # install isolate
 sudo bash script/install/cgroup.bash || exit 1 # install cgroups
