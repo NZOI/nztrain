910adf3e89a4468217c9438b276b504b08f08093
Tom Levy <tomlevy93@gmail.com>
2020-05-09 01:11:38 +1200

    Remove include of `rspec/autorun`
    Deprecation warning fixed:
    
    "Requiring `rspec/autorun` when running RSpec via the `rspec` command is deprecated."
    
    It is still possible to run specs using the `ruby` command, but the
    `-rrspec/autorun` option must now be used as described in
    https://relishapp.com/rspec/rspec-core/v/3-9/docs/command-line:
    
        ruby -Ilib -Ispec -rrspec/autorun path/to/spec.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ Coveralls.wear!('rails')
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/email/rspec'
 require "pundit/rspec"
