e4c35fef1858ff772745fd2cc6c196377c0922f9
Jonathan Khoo <jkhoo98@gmail.com>
2021-02-07 00:02:06 +1300

    Remove schools of users outside New Zealand

---
+++ db/migrate/20210206104409_remove_schools_for_foreign_users.rb
@@ -0,0 +1,13 @@
+class RemoveSchoolsForForeignUsers < ActiveRecord::Migration
+  def up
+    User.find_each do |user|
+      if user.country_code != "NZ" && user.school
+        user.school = nil
+        user.save
+      end
+    end
+  end
+
+  def down
+  end
+end
