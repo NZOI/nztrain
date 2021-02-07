0ff54a95d6af7eccbe1b3236d6d7e0b979c65a14
Tom Levy <tomlevy93@gmail.com>
2021-02-08 02:25:40 +1300

    Fix indentation

--- db/migrate/20200418113601_add_offical_contestants.rb
+++ db/migrate/20200418113601_add_offical_contestants.rb
@@ -1,5 +1,5 @@
 class AddOfficalContestants < ActiveRecord::Migration
-    def change
-      add_column :contests, :only_rank_official_contestants, :boolean, :default => false
-    end
+  def change
+    add_column :contests, :only_rank_official_contestants, :boolean, :default => false
   end
+end
