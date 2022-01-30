2a8316f94f5ceb497604465e3adac1cd0e5b8d4a
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2022-01-30 19:05:38 +1300

    Expose GET /accounts/requests.xml

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
-  
+
   belongs_to :requester, :class_name => :User # entity that initiated request
   belongs_to :subject, :polymorphic => true # subject controlled by requester
   # verb # action applying subject to target
@@ -41,5 +41,19 @@ class Request < ActiveRecord::Base
   def cancel!
     self.status = STATUS[:cancelled]
     self.save
+  end
+
+  def to_xml(opts={})
+    if self.expired_at == Float::INFINITY then
+      # `expired_at` has the value Float::INFINITY when the request hasn't expired,
+      # and the XML formatter explodes when it encounters that value. Adding :expired_at
+      # to opts[:exclude] is the obvious solution, but for reasons unknown to me it does
+      # not appear to work as expected. Changing `expired_at` to some other value (which isn't
+      # a datetime) will result in an empty tag being emitted with a `nil="true"` attribute
+      # which seems like the best solution after just omitting the tag entirely.`
+      self.expired_at = 0
+    end
+
+    super(opts)
   end
 end
