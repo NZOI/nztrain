c3fe406b989855f0cfe11bdeafb0946c22f63c0c
Tom Levy <tomlevy93@gmail.com>
2023-02-16 16:12:42 +1300

    Make forum spec slightly more realistic

--- spec/requests/legacy_routes_request_spec.rb
+++ spec/requests/legacy_routes_request_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe "Legacy routes maintained for compat" do
   describe "Forem removal / replacement" do
-    ["/forum", "/forum/", "/forum/bob/your/uncle"].each do |path|
+    ["/forum", "/forum/", "/forum/news/topics/welcome-to-forem"].each do |path|
       describe path do
         it "renders the forum deprecated page" do
           get path
