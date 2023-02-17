63ad0a26952940111e713cbb92ee3cf50bcfa431
Tom Levy <tomlevy93@gmail.com>
2023-02-17 18:48:33 +1300

    Regenerate db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -156,56 +156,6 @@ ActiveRecord::Schema.define(version: 20200418113601) do
   add_index "filelinks", ["file_attachment_id"], name: "index_filelinks_on_file_attachment_id", using: :btree
   add_index "filelinks", ["root_id", "filepath"], name: "index_filelinks_on_root_id_and_filepath", using: :btree
 
-  create_table "forem_categories", force: true do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "forem_forums", force: true do |t|
-    t.string "title"
-    t.text   "description"
-  end
-
-  create_table "forem_posts", force: true do |t|
-    t.integer  "topic_id"
-    t.text     "text"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "reply_to_id"
-  end
-
-  add_index "forem_posts", ["reply_to_id"], name: "index_forem_posts_on_reply_to_id", using: :btree
-  add_index "forem_posts", ["topic_id"], name: "index_forem_posts_on_topic_id", using: :btree
-  add_index "forem_posts", ["user_id"], name: "index_forem_posts_on_user_id", using: :btree
-
-  create_table "forem_topics", force: true do |t|
-    t.integer  "forum_id"
-    t.integer  "user_id"
-    t.string   "subject"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "locked",     default: false, null: false
-    t.boolean  "pinned",     default: false
-    t.boolean  "hidden",     default: false
-  end
-
-  add_index "forem_topics", ["forum_id"], name: "index_forem_topics_on_forum_id", using: :btree
-  add_index "forem_topics", ["user_id"], name: "index_forem_topics_on_user_id", using: :btree
-
-  create_table "forem_views", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "topic_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "count",      default: 0
-  end
-
-  add_index "forem_views", ["topic_id"], name: "index_forem_views_on_topic_id", using: :btree
-  add_index "forem_views", ["updated_at"], name: "index_forem_views_on_updated_at", using: :btree
-  add_index "forem_views", ["user_id"], name: "index_forem_views_on_user_id", using: :btree
-
   create_table "group_contests", force: true do |t|
     t.integer "group_id"
     t.integer "contest_id"
