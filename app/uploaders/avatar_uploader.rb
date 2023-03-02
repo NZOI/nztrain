d8a453b47120fcfc96ed6e02b180beb6ca7589c9
Tom Levy <tomlevy93@gmail.com>
2023-03-02 18:31:44 +1300

    Remove unused forum stylesheet (#179)
    The forum was removed in commit 29bfab38 (Drop forem, replace with
    placeholder page to avoid dead links (#161), 2023-02-19).

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -37,7 +37,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [150, 150] # suitable for forum size
+  process :resize_to_fit => [150, 150] # suitable for profile page
   #
   # def scale(width, height)
   #   # do something
