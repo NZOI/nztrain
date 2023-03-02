2be08f400adbc153b17010415da409ea7b4a4911
Ben Anderson <benanderson@acidic.co.nz>
2023-03-02 20:53:56 +1300

    Remove data changes in older migrations.
    Means you can migrate from zero -> now without any issues. Only the
    "data only" aspects of the migrations were changed, preserving all
    schema changes. This ensures we can migrate from start to finish
    cleanly, and that our migrations aren't relying on schema or data that
    won't be present in a clean database.
    
    More generally, this is why people introduce tools like data-migrate [1]
    and avoid using models in the their regular migrations, but hey.
    
    [1] https://github.com/ilyakatz/data-migrate

--- db/migrate/20131206074948_name_all_test_sets_and_cases.rb
+++ db/migrate/20131206074948_name_all_test_sets_and_cases.rb
@@ -1,12 +1,8 @@
-require 'set'
-
 class NameAllTestSetsAndCases < ActiveRecord::Migration
-  def up
-    Problem.find_each do |problem|
-      name_uniquely(problem.test_sets)
-      name_uniquely(problem.test_cases)
-    end
+  # Some contents of this migration have been removed to ensure
+  # we can always migrate cleanly.
 
+  def up
     add_index :test_sets, [:problem_id, :name], :unique => true
     add_index :test_cases, [:problem_id, :name], :unique => true
   end
@@ -14,19 +10,5 @@ class NameAllTestSetsAndCases < ActiveRecord::Migration
   def down
     remove_index :test_sets, [:problem_id, :name]
     remove_index :test_cases, [:problem_id, :name]
-  end
-
-  def name_uniquely relation
-    klass = relation.klass
-    names = relation.pluck(:name).compact.reject{|s|s.blank?}
-    names = Set[*names] - names.group_by { |e| e }.select { |k, v| v.size > 1 }.map(&:first)
-    i = 1
-    relation.select([:id, :name]).each do |obj|
-      if !names.include?(obj.name)
-        i+=1 while names.include?(i.to_s)
-        klass.update(obj.id, :name => i.to_s)
-        i+=1
-      end
-    end
   end
 end
