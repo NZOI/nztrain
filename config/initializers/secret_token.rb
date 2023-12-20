3794d2728b48df851e6627ec37b38af9caef8297
Tom Levy <tomlevy93@gmail.com>
2023-12-21 02:40:29 +1300

    Add config.secret_key_base (#195)
    secret_key_base was added in Rails 4 and replaces secret_token. It is
    used by CookieStore to encrypt cookies (we are not currently using
    CookieStore, but we will switch to it in #162).
    
    The Rails 4 convention for setting secret_key_base is to use
    config/secrets.yml and the environment variable SECRET_KEY_BASE (see
    [1]). We don't use this approach, because secrets.yml will be
    deprecated in favour of credentials.yml.enc in Rails 5.2 [2] and
    because setting the environment variable is awkward.
    
    Instead, we generate a random secret the first time the app runs and
    store it in config/secret_key_base.#{env}.txt. We also support the
    environment variable SECRET_KEY_BASE to match the Rails convention; we
    don't currently use this environment variable, but it may be useful
    (e.g. in situations such as build steps where we don't want to
    generate the file, or in environments such as Docker where environment
    variables are often more convenient than files).
    
    We don't store the secret in the database (which is how the existing
    secret_token is implemented) because it may be useful to start the app
    without a database connection. Also, it's probably more secure to keep
    the secret out of the database (and its backups).
    
    This commit also adds a rule to .gitignore to make sure the secrets
    aren't accidentally committed.
    
    [1]: https://guides.rubyonrails.org/v4.1.8/upgrading_ruby_on_rails.html#config-secrets-yml
    [2]: https://www.github.com/rails/rails/pull/30067

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,3 +12,23 @@ if ActiveRecord::Base.connection.table_exists?(Setting.table_name) && Setting.ex
   token = Setting.find_by_key("sessions/secret_token").value
   NZTrain::Application.config.secret_token = token if token && !(token.empty?)
 end
+
+# The secret_key_base is used as the input secret to the
+# application's key generator, which is used to sign and encrypt
+# cookies.
+#
+# Once we upgrade to Rails 5.2 we might remove this code and instead
+# set secret_key_base using config/credentials.yml.enc or using the
+# environment variable SECRET_KEY_BASE (which is natively supported
+# by the Rails 5.2 method Rails::Application.secret_key_base).
+NZTrain::Application.config.secret_key_base ||= ENV["SECRET_KEY_BASE"]
+if NZTrain::Application.config.secret_key_base.nil?
+  # Randomly generate a secret and store it in config/secret_key_base.#{env}.txt
+  # Code inspired by Rails.application.generate_local_secret
+  # (https://github.com/rails/rails/blob/v7.1.2/railties/lib/rails/application.rb#L665)
+  file = Rails.root.join("config/secret_key_base.#{Rails.env}.txt")
+  if !File.exist?(file)
+    File.binwrite(file, SecureRandom.hex(64))
+  end
+  NZTrain::Application.config.secret_key_base = File.binread(file).strip
+end
