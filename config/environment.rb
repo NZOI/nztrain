63d2cb2eee62180b478763be33c5b3ab8ebf0261
Tom Levy <tomlevy93@gmail.com>
2023-12-22 19:27:20 +1300

    Remove unused String.shorten extension
    It's only used in one view, and that view is unused and broken.
    
    And anyway, Rails 3 has a built-in truncate method that we can use
    instead.
    
    Suggested-by: Ben Anderson <benanderson@acidic.co.nz>

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'ext/string'
-#longest length a string can be before it's truncated in index view
-SHORTEN_LIMIT = 100
-
 #Initialize the rails application
 NZTrain::Application.initialize!
