6c64bcc0fe1340b462b89b17dfdd420ce9b4b673
Ben Anderson <ben@delivereasy.co.nz>
2023-12-22 20:10:17 +1300

    Remove explicit setting of config.encoding
    The value we are setting was made default in Rails 3, at least [1].
    This line was removed from the template in Rails 4 [2].
    
    [1] https://github.com/rails/rails/commit/ec73710c79c5e1587b489b2ce05cc34138acf071
    [2] https://github.com/rails/rails/commit/daf5b66a116ba71f9f917ec98c9ddc83a35ed3ae
    
    Co-authored-by: Tom Levy <tomlevy93@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,6 @@ module NZTrain
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
