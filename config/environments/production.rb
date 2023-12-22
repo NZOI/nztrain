77fd3ff8cf4720dde7a80db59e337ffcf5aa3ab4
Tom Levy <tomlevy93@gmail.com>
2023-12-23 00:33:52 +1300

    Reorder config/environments/production.rb to match Rails 3.0.0/4.0.0
    No semantic changes. (Added Rails 4 comment for config.force_ssl.)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,31 @@
 NZTrain::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.force_ssl = true
-  
-  # Eager load all gems and libraries
-  config.eager_load = true
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Eager load all gems and libraries
+  config.eager_load = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+  # Disable Rails's static asset server
+  # In production, Apache or nginx will already do this
+  config.serve_static_assets = true # true so Webrick testing of production mode works
+
+  # Compress JavaScript and CSS
+  config.assets.compress = true
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglifier
+
+  # Don't fallback to assets pipeline
+  # config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile"
@@ -22,6 +36,9 @@ NZTrain::Application.configure do
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
@@ -30,10 +47,6 @@ NZTrain::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = true # true so Webrick testing of production mode works
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -50,15 +63,4 @@ NZTrain::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # Compress JavaScript and CSS  
-  config.assets.compress = true  
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglifier
- 
-  # Don't fallback to assets pipeline  
-  # config.assets.compile = false  
-     
-  # Generate digests for assets URLs  
-  config.assets.digest = true  
 end
