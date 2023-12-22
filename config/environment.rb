197a7768269bf4b861aed6e1dc2218c79eb08f22
Tom Levy <tomlevy93@gmail.com>
2023-12-22 16:47:18 +1300

    Remove unused require of 'rbconfig' (#203)
    All references to RbConfig are gone.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'rbconfig'
-
 require 'ext/string'
 #longest length a string can be before it's truncated in index view
 SHORTEN_LIMIT = 100
