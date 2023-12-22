94319daba0b515ae4e7944cb5a8a0672090cf7b0
Tom Levy <tomlevy93@gmail.com>
2023-12-22 16:41:23 +1300

    Remove unused variable HIGH_SCORE_LIMIT (#203)
    Unused since commit 6ba5832a (caching contest scores in extra table,
    ..., 2012-08-07).

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,6 @@ require 'ext/string'
 #longest length a string can be before it's truncated in index view
 SHORTEN_LIMIT = 100
 
-#value between 0 and 1, used to reduce fraction of competitors shown on high-score table
-HIGH_SCORE_LIMIT = 0.5
-
 #ActiveRecord::Base.pluralize_table_names = false
 
 #Initialize the rails application
