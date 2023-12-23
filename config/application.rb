b53e834afd547c012c133620e04d134c9c087629
Jonathan Khoo <jkhoo98@gmail.com>
2023-12-23 19:16:13 +1300

    Reset to default Rails 4.2 config

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../boot', __FILE__)
-require 'rubygems'
+
 require 'rails/all'
 
-if defined?(Bundler)  
-  # Require the gems listed in Gemfile, including any gems
-  # you've limited to :test, :development, or :production.
-  Bundler.require(:default, Rails.env)
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module NZTrain
   class Application < Rails::Application
@@ -14,12 +12,9 @@ module NZTrain
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{Rails.root}/lib)
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Auckland'
+    # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -27,37 +22,5 @@ module NZTrain
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-    config.assets.paths << Rails.root.join("fonts")
-    config.assets.precompile += %w(submission.css)
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-
-    config.action_mailer.default_url_options = { :host => 'train.nzoi.org.nz' }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default :charset => "utf-8"
-
-    ActionMailer::Base.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :authentication => :plain,
-      :domain => 'nztrain.com',
-      :user_name => 'nztrain@gmail.com', # username and password set with higher priority in settings table
-      :password => 'training site',
-      :enable_starttls_auto => true
-    }
-
-    config.middleware.use PDFKit::Middleware, {}, :only => [%r[^/item/[0-9]*/label]]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,239 +1,20 @@
-NZTrain::Application.routes.draw do
-  concern :file_root do |options|
-    options ||= {}
-    resources :files, options.merge(:except => [:new, :edit]) do
-      collection do
-        get 'download/(*filepath)', action: :show, as: :download, defaults: {filepath: nil}
-      end
-    end
-  end
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
-  root :to => "home#home"
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
 
-  require 'qless/server'
-  authenticate :user, ->(current_user) {current_user.is_admin?} do
-    get 'qless', :to => 'qless#default', :as => 'qless'
-    #constraints ->(request) {request.get?} do
-    mount Qless::Server.new($qless) => '/qless/server', :as => 'qless_server' # cannot restrict post requests anymore
-    #end
-  end
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
 
-  #resources :test_sets
-  #resources :test_cases
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  resources :evaluators
-
-  resources :settings
-
-  resources :products
-
-  resources :roles
-
-  resources :contests do
-    collection do
-      get 'my', :to => 'contests#index', :defaults => { :filter => 'my' }
-      get 'active', :to => 'contests#browse', :defaults => { :filter => 'active' }
-      get 'current', :to => 'contests#browse', :defaults => { :filter => 'current' }
-      get 'upcoming', :to => 'contests#browse', :defaults => { :filter => 'upcoming' }
-      get 'past', :to => 'contests#browse', :defaults => { :filter => 'past' }
-    end
-    member do
-      get 'info'
-      get 'scoreboard'
-      get 'contestants'
-      get 'supervisors'
-      post 'supervise'
-
-      get 'export'
-
-      #get 'start'
-      put 'start'
-      put 'finalize'
-      put 'unfinalize'
-      put 'register'
-      post 'register'
-    end
-  end
-
-  # resources :contest_relations
-  resources :contest_relations, only: :destroy do
-    member do
-      post 'update_year_level'
-    end
-  end
-
-  resources :contest_supervisors, only: [:create, :destroy] do
-    member do
-      patch 'update_scheduled_time'
-    end
-  end
-
-  resources :submissions, :except => [:new,:create] do
-    collection do
-      get '(by_user/:by_user)(/by_problem/:by_problem)', :action => :index, :constraints => {:by_user => /\d+/, :by_problem => /\d+/}
-      get 'my', :to => 'submissions#index', :defaults => { :filter => 'my' }
-    end
-    member do
-      post 'rejudge'
-    end
-  end
-
-  resources :problems do
-    collection do
-      get 'my', :to => 'problems#index', :defaults => { :filter => 'my' }
-    end
-    member do
-      post 'submit'
-      get 'submit'
-      get 'submissions'
-
-      get 'export'
-      post 'import'
-
-      get 'testing'
-
-      put 'remove_from_problem_set'
-    end
-    resources :test_cases, :module => :problems, :only => [:index] do
-      patch '', action: :update, on: :collection
-    end
-    concerns :file_root, :module => :problems
-  end
-
-  resources :problem_sets, except: [:index] do
-    collection do
-      get '', :action => :index, :as => '' # manually specify so that the primary #index route is not 'my'
-      get 'my', :action => :index, :defaults => { :filter => 'my' }
-    end
-    member do
-      put 'add_problem'
-      put 'remove_problem'
-    end
-  end
-
-  resources :users, :only => :index do
-    collection do
-      post 'suexit'
-
-      get 'online'
-      get 'newest'
-    end
-  end
-
-  resources :user, :only => [:index, :show, :edit, :update, :destroy] do
-    member do
-      put 'add_role'
-      put 'remove_role'
-      post 'su'
-      get 'su'
-      get 'admin_email'
-      post 'admin_email', :action => :send_admin_email
-      post 'add_brownie'
-    end
-  end
-
-  devise_for :users, :path => "accounts", :controllers => { :registrations => "accounts/registrations", :settings => "accounts/settings", :confirmations => "accounts/confirmations", :passwords => "accounts/passwords" }
-
-  devise_scope :user do
-    namespace :accounts do
-      get 'edit/:type', :to => 'registrations#edit'
-      put 'update/:type', :to => 'registrations#update'
-      get 'settings/edit', :to => 'settings#edit'
-      put 'settings/update', :to => 'settings#update'
-
-      get 'requests', :to => 'requests#index'
-    end
-  end
-
-  put 'problem_problem_set/add' => "problem_problem_set#add"
-  put 'problem_problem_set/remove' => "problem_problem_set#remove"
-
-  put 'group_problem_set/add' => "group_problem_set#add"
-
-  resources :groups do
-    collection do
-      get 'my', :to => 'groups#index', :defaults => { :filter => 'my' }
-      get 'browse'
-
-      put 'add_problem_set'
-      put 'add_contest'
-    end
-    member do
-      get 'contests'
-      get 'info'
-      get 'scoreboard'
-
-      scope path: :members, module: :groups, controller: :members do
-        put 'join'
-        put 'leave'
-        put 'apply'
-
-        scope as: :members do
-          get '', action: :index, as: ''
-
-          post 'add'
-          put 'remove'
-
-          get 'invites'
-          post 'invites'
-          get 'join_requests'
-
-          put 'accept/:request_id', action: :accept, as: :accept
-          put 'reject/:request_id', action: :reject, as: :reject
-          put 'cancel/:request_id', action: :cancel, as: :cancel
-        end
-      end
-
-      put 'remove_contest'
-    end
-    concerns :file_root, :module => :groups
-    resources :problem_sets, :module => :groups, :only => [:show, :update, :destroy] do
-      get 'edit', on: :collection
-    end
-  end
-
-  resources :file_attachments do
-    collection do
-      get 'my', :to => 'file_attachments#index', :defaults => { :filter => 'my' }
-    end
-    member do
-      get 'download'
-    end
-  end
-
-  namespace :importers do
-    get 'problem_series/:series/', to: 'problem_series#index', as: 'problem_series'
-    %w[download reindex].each do |operation|
-      post "problem_series/:series(/:vid(/:cid))/#{operation}", to: "problem_series##{operation}", as: "#{operation}_problem_series"
-    end
-    patch 'problem_series/:series/:vid(/:cid)', to: "problem_series#update", as: "update_problem_series"
-    post 'problem_series/:series/update_index', to: "problem_series#update_index", as: "update_index_problem_series"
-  end
-
-  resources :organisations do
-    member do
-      get 'product/:product_id', to: "organisations#product", as: "product"
-      get 'product/:product_id/new_product_item', to: "organisations#new_product_item", as: "new_product_item"
-      post 'product/:product_id', to: "organisations#create_product_item", as: "create_product_item"
-    end
-  end
-
-  resources :schools, only: [:index, :show, :edit, :update, :destroy]
-
-  resources :items, only: :index
-  resources :item, except: [:index, :create, :new] do
-    member do
-      get 'label'
-      get 'scan/:scan_token', to: "item#scan", as: "scan" # public access
-      post 'loan'
-      post 'return'
-    end
-  end
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Sample resource route with options:
+  # Example resource route with options:
   #   resources :products do
   #     member do
   #       get 'short'
@@ -245,33 +26,31 @@ NZTrain::Application.routes.draw do
   #     end
   #   end
 
-  # Sample resource route with sub-resources:
+  # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
   #   end
 
-  # Sample resource route with more complex sub-resources
+  # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
   #     resources :sales do
-  #       get 'recent', :on => :collection
+  #       get 'recent', on: :collection
   #     end
   #   end
 
-  # Sample resource route within a namespace:
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Legacy routes maintained to avoid routes breaking
-  get "/forum/(*anything)" => "pages#forum"
 end
