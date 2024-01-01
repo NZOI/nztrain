20e9938de9a14b99b2119b7fdcc6deda8e54ced2
Tom Levy <tomlevy93@gmail.com>
2024-01-02 02:53:38 +1300

    Add env var DATABASE_INSTALL to skip PostgreSQL install (#237)
    If the database is running on another host/container, then we don't
    need to install the PostgreSQL server on the web server.
    
    This is useful for CI, and mirrors the existing REDIS_INSTALL
    environment variable.
    
    We always need the PostgreSQL client utilities, so install them
    separately.
    
    If we skip installation of the database then we don't try to create
    the database user, because that's unlikely to work with remote
    databases.

--- script/install/postgresql.bash
+++ script/install/postgresql.bash
@@ -4,28 +4,38 @@ cd `dirname $0`/../..
 
 source script/install.cfg
 
-id -u postgres &> /dev/null || {
-  echo "PostgreSQL required!"
-  bash script/confirm.bash 'Install PostgreSQL' && {
-    cmd="sudo apt-get install postgresql"
-    echo "$ $cmd"
-    $cmd
-  } || exit 1
-}
-
-# required by pg gem
+# header files (required by pg gem)
 cmd="sudo apt-get install libpq-dev"
 echo "$ $cmd"
 $cmd || exit 1
 
-# setup user if required
-psql -U "$DATABASE_USERNAME" postgres -c '' &> /dev/null || {
-  bash script/confirm.bash "Create new PostgreSQL user $DATABASE_USERNAME" && {
-    cmd="sudo -u postgres createuser --superuser $DATABASE_USERNAME"
-    echo "$ $cmd"
-    $cmd
-  } || exit 1
+# client utilities (psql, createdb, pg_dump, etc.)
+command -v psql >/dev/null || {
+  cmd="sudo apt-get install postgresql-client"
+  echo "$ $cmd"
+  $cmd || exit 1
 }
+
+if [ "${DATABASE_INSTALL:-true}" = "true" ]; then
+  # install server (not required if the server is running on another host/container)
+  id -u postgres &> /dev/null || {
+    echo "PostgreSQL server required!"
+    bash script/confirm.bash 'Install PostgreSQL server' && {
+      cmd="sudo apt-get install postgresql"
+      echo "$ $cmd"
+      $cmd
+    } || exit 1
+  }
+
+  # setup user if required
+  psql -U "$DATABASE_USERNAME" postgres -c '' &> /dev/null || {
+    bash script/confirm.bash "Create new PostgreSQL user $DATABASE_USERNAME" && {
+      cmd="sudo -u postgres createuser --superuser $DATABASE_USERNAME"
+      echo "$ $cmd"
+      $cmd
+    } || exit 1
+  }
+fi
 
 # setup database if required
 if [[ $DATABASE ]] ; then
