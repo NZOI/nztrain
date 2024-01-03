7ecce6d70c09ff1eed8348e9b8c49eb73213c9e9
Tom Levy <tomlevy93@gmail.com>
2024-01-04 02:35:22 +1300

    Skip wkhtmltopdf in CI (#255)
    It is used by the PDFKit gem, which is only used from
    app/views/item/label.html.erb. That view is not tested, so we can make
    CI faster by skipping the install of wkhtmltopdf. (We can add it back
    if/when we write a test for the view.)

--- script/install.bash
+++ script/install.bash
@@ -34,7 +34,7 @@ bash script/install/postgresql.bash || exit 1 # create user & db if it doesn't e
 
 bash script/install/imagemagick.bash || exit 1 # install imagemagick
 
-sudo apt-get install wkhtmltopdf xvfb || exit 1
+[ -n "$CI" ] || sudo apt-get install wkhtmltopdf xvfb || exit 1 # for pdfkit; skip in CI
 
 bash script/install/nztrain.bash || exit 1 # fix files & directory structure
 
