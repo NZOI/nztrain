4034cd7cded8b4d1f3d5707d3db74003c2699afc
Tom Levy <tomlevy93@gmail.com>
2025-03-03 00:35:15 +1300

    Set "from" email address using settings table instead of hard-coding
    Previously the "from" address was hard-coded to nztrain@gmail.com and
    could not be configured.
    
    This commit uses the value of the "system/mailer/username" setting,
    which is the user name for SMTP authentication. It will be the same as
    the "from" email when using Gmail and in most other setups. (A later
    commit will rename that setting to "system/mailer/email".)
    
    * We provide a default email in the test environment, otherwise the
      specs fail.
    * We don't provide a default email in the production environment,
      because the admin must configure email properly in order
      for mail delivery to work.
    * We don't provide a default email in the development environment,
      because mail delivery will fail anyway (unless SMTP is configured
      properly). The admin will still be able to read the emails by
      looking in the development log.
    
    In the future we may wish to add a setting for overriding the
    "Reply-To" address; it could be useful for redirecting responses to an
    mailbox that's actually monitored
    (https://www.github.com/NZOI/nztrain/pull/272#issuecomment-2704959896).
    But for now we don't bother.

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,7 +1,8 @@
-# use db stored username and password for email
+# use db-stored email address and password for sending email
 
 if ActiveRecord::Base.connection.table_exists?(Setting.table_name)
   ActionMailer::Base.smtp_settings[:user_name] = Setting.find_by_key("system/mailer/username")&.value
   ActionMailer::Base.smtp_settings[:password] = Setting.find_by_key("system/mailer/password")&.value
+  ActionMailer::Base.default from: Setting.find_by_key("system/mailer/username").value if Setting.find_by_key("system/mailer/username")&.value.present?
 end
 
