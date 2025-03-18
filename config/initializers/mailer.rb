d3d216cd11191062a11104f70b1900bf60aa79a2
Tom Levy <tomlevy93@gmail.com>
2025-03-18 19:45:22 +1300

    Set email sender's display name to "NZOI Training"
    Ideally it should be configurable, but we hard-code it for now like in
    other places for simplicity.
    
    Suggested-by: Jonathan Khoo <jkhoo98@gmail.com>

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,8 +1,17 @@
 # use db-stored email address and password for sending email
 
+def email_address_with_name(address, name)
+  # can switch to ActionMailer::Base.email_address_with_name() once we upgrade to Rails 6
+  Mail::Address.new.tap do |builder|
+    builder.address = address
+    builder.display_name = name.presence
+  end.to_s
+end
+
 if ActiveRecord::Base.connection.table_exists?(Setting.table_name)
-  ActionMailer::Base.smtp_settings[:user_name] = Setting.find_by_key("system/mailer/email")&.value
+  email_setting = Setting.find_by_key("system/mailer/email")
+  ActionMailer::Base.smtp_settings[:user_name] = email_setting&.value
   ActionMailer::Base.smtp_settings[:password] = Setting.find_by_key("system/mailer/password")&.value
-  ActionMailer::Base.default from: Setting.find_by_key("system/mailer/email").value if Setting.find_by_key("system/mailer/email")&.value.present?
+  ActionMailer::Base.default from: email_address_with_name(email_setting.value, "NZOI Training") if email_setting&.value.present?
 end
 
