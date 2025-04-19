41073c0bc5b29d97e6c7b749e6f2a76fad47bdc3
Ben Anderson <me@benanderson.nz>
2025-04-19 22:18:55 +1200

    Write out FixturesSpecHelper
    Having this helper generate these records before the test suite runs
    means they are not included any the standard rspec transactional tests
    mechanism. This means that if the suite cannot fully complete for
    any reason, such as a syntax error, or if the number of create/destroy
    is not perfectly matched in a test then the following test run will
    fail.
    
    Using let(:x) and before is best practise, and shouldn't have a
    significant effect on test runtimes.

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -1,25 +1,26 @@
 require "spec_helper"
 
 describe SettingsController do
-  before(:all) do
-    @setting = FactoryBot.create(:setting)
-  end
-  after(:all) do
-    @setting.destroy
+  let(:setting) { FactoryBot.create(:setting) }
+
+  before do
+    sign_in user
   end
 
   context "as superadmin" do
-    before(:each) do
-      sign_in users(:superadmin)
-    end
+    let(:user) { FactoryBot.create(:superadmin) }
+
     can_index :settings
     can_create :setting, attributes: {key: "A unique name", value: "Secret value setting"}
     can_manage :setting, attributes: {key: "A unique name", value: "Secret value setting"}
   end
 
   context "as a normal user" do
-    before(:each) do
-      sign_in users(:user)
+    let(:user) { FactoryBot.create(:user) }
+
+    it "doesn't display" do
+      get :index
+      expect(response).to have_http_status(:forbidden)
     end
   end
 end
--- spec/models/contest_relation_spec.rb
+++ spec/models/contest_relation_spec.rb
@@ -1,7 +1,6 @@
 require "spec_helper"
 
 describe ContestRelation do
-  include FixturesSpecHelper
   RSpec::Matchers.define :finish_at_correct_time do
     def expected_time(relation)
       [relation.started_at.advance(hours: relation.contest.duration), relation.contest.end_time].min
@@ -19,34 +18,34 @@ describe ContestRelation do
       "finish at the earlier of when contest duration runs out or when contest finishes"
     end
   end
-  before(:all) do
-    @contest = FactoryBot.create(:contest)
-    @relation = FactoryBot.create(:contest_relation, contest: @contest, user: users(:user))
-  end
-  after(:all) do
-    @relation.destroy
-    @contest.destroy
-  end
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:contest) { FactoryBot.create(:contest) }
+  let(:relation) { FactoryBot.create(:contest_relation, contest: contest, user: user) }
+
   it "updates finish_at when relation started_at changes" do
-    expect(@relation).to finish_at_correct_time
-    @relation.started_at = @contest.end_time.advance(hours: -1)
-    expect(@relation).to finish_at_correct_time
+    expect(relation).to finish_at_correct_time
+    relation.started_at = contest.end_time.advance(hours: -1)
+    expect(relation).to finish_at_correct_time
   end
+
   it "updates finish_at when contest changes" do
-    @anothercontest = FactoryBot.build(:contest, start_time: @relation.started_at.advance(hours: -1), end_time: @relation.started_at.advance(hours: 1))
-    @relation.contest = @anothercontest
-    expect(@relation).to finish_at_correct_time
-    @relation.contest_id = @contest.id
-    expect(@relation).to finish_at_correct_time
+    anothercontest = FactoryBot.build(:contest, start_time: relation.started_at.advance(hours: -1), end_time: relation.started_at.advance(hours: 1))
+    relation.contest = anothercontest
+    expect(relation).to finish_at_correct_time
+    relation.contest_id = contest.id
+    expect(relation).to finish_at_correct_time
   end
+
   it "updates finish_at when contest end_time changes" do
-    @contest.update_attributes(end_time: @relation.started_at.advance(hours: 1))
-    expect(@relation).to finish_at_correct_time
+    contest.update_attributes(end_time: relation.started_at.advance(hours: 1))
+    expect(relation.reload).to finish_at_correct_time
   end
+
   it "updates finish_at when contest duration changes" do
-    @contest.update_attributes(duration: 1.0)
-    expect(@relation).to finish_at_correct_time
-    @contest.update_attributes(duration: 5.0)
-    expect(@relation).to finish_at_correct_time
+    contest.update_attributes(duration: 1.0)
+    expect(relation.reload).to finish_at_correct_time
+    contest.update_attributes(duration: 5.0)
+    expect(relation.reload).to finish_at_correct_time
   end
 end
