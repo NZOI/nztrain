1fb498a4a3a65cd7fc00a64e82c254103c9d9634
Ben Anderson <ben@delivereasy.co.nz>
2025-07-20 21:57:41 +1200

    Change all has_many / belongs_to class_name attributes to strings
    Rails 5.1 deprecated non-strings for this value, so this just moves them
    all over.

--- app/models/evaluator.rb
+++ app/models/evaluator.rb
@@ -2,7 +2,7 @@ class Evaluator < ApplicationRecord
   include ActiveModel::ForbiddenAttributesProtection
 
   has_many :problems
-  belongs_to :owner, class_name: :User
+  belongs_to :owner, class_name: "User"
   belongs_to :language
 
   validates :name, presence: true
