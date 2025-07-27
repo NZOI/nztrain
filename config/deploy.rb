01e4e8fb229a15db8c64ce5d4c304a7b47841fef
Ben Anderson <me@benanderson.nz>
2025-07-27 16:30:38 +1200

    Allow overriding the deploy branch during a cap deploy (#346)
    By specifying DEPLOY_BRANCH=<branch_name> a different branch will be
    deployed to the specified environment, instead of the default master.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :repo_url, "git@github.com:NZOI/nztrain.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV.fetch("DEPLOY_BRANCH", "master")
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
