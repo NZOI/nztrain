1feb6f1393a3e75995612f164ff08be2be65bf2b
Salamander <salamanderhash0514@gmail.com>
2025-11-26 19:09:57 +1300

    Reset column information after adding enum to fix migration crashing issue

--- db/migrate/20250902233414_add_scoring_method_to_problem.rb
+++ db/migrate/20250902233414_add_scoring_method_to_problem.rb
@@ -3,6 +3,7 @@ class AddScoringMethodToProblem < ActiveRecord::Migration
     add_column :problems, :scoring_method, :integer, default: 1 # 0: max-submission, 1: subtask-scoring
 
     # Set all current problems to use max-submission
+    Problem.reset_column_information
     Problem.update_all(scoring_method: :max_submission_scoring)
   end
 end
